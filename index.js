2c20e23ec3c346732359ce7b4cf31024a6ced1b9
Aseem7905H <targetaseem@gmail.com>
2025-06-11 23:25:05 +0530

    env added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express"
+require('dotenv').config() ; // Load environment variables from .env file
 
 const app = express() 
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.json());
 
  app.get('/', (req,res) => {
